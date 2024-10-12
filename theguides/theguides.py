e0de0cd5fe10681e1ecae0e54d34bc997da69180
Felix <felix@detry.com.ar>
2024-10-12 18:18:39 +0100

    Fix error handling and remove debug print statement
    Changed error handling from `break` to `continue` to ensure all entries are processed. Commented out a debug print statement to reduce noise in the output.

--- theguides/theguides.py
+++ theguides/theguides.py
@@ -77,8 +77,8 @@ async def rank_users_by_tickets_this_month_to_csv(pool, ctx):
                     if roblox_data["error"] == "Unknown Member":
                         await ctx.channel.send(f"Discord ID: {i[0]} not in discord, <@{i[0]}> will not be included in pay, but if you need his ticket amount it is: `{i[1]}`")
                         print(f"{i[0]} NOT IN DISCORD, NOT INCLUDED IN CSV")
-                        break
-                print(roblox_data)
+                        continue
+                #print(roblox_data)
                 roblox_name = roblox_data["resolved"]["roblox"]["name"]
                 print(f"Resolved: {roblox_name} for DISCORD_ID: {i[0]}")
 
